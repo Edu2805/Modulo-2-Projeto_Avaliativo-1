e77b7231c035d82de8dea0ca7b7cc7d52221f4b1
Eduardo <eduardoamorim927@gmail.com>
2022-04-27 22:53:54 -0300

    Foram feitos ajustes nas funcionalidades de soma e subtração de valor de produtos

--- lista.js
+++ lista.js
@@ -1,4 +1,3 @@
-// Seletores
 const productInput = document.querySelector("#product-input");
 const insertButton = document.querySelector(".product-button");
 const productList = document.querySelector(".products-list");
@@ -6,18 +5,24 @@ const priceInput = document.querySelector("#price-input");
 const priceButton = document.querySelector(".price-button");
 var totalValue = document.querySelector("p");
 
+var productArray = [];
+var productObj = {}
+//####################################################################################
+
 var initialValue = parseFloat(0);
 totalValue.innerText = "R$ " + initialValue.toFixed(2);
 var totalValueList = 0;
 var totalFinalValueList = 0;
 
-//Eventos
+//####################################################################################
+
 document.addEventListener("DOMContentLoaded", getProducts);
 insertButton.addEventListener("click", addProduct);
-productList.addEventListener("click", deleteProduct);
+productList.addEventListener("click", productInteration);
 priceButton.addEventListener("click", sendPrice);
 
-//Funções
+//####################################################################################
+
 function addProduct(event) {
   event.preventDefault();
 
@@ -46,27 +51,37 @@ function addProduct(event) {
     const newProduct = document.createElement("li");
     newProduct.innerText = productInput.value;
     newProduct.classList.add("product-unit");
-
     productDiv.appendChild(newProduct);
 
     addLocalProducts(productInput.value);
 
     productList.appendChild(productDiv);
 
+    productObj = {
+      productName: productInput.value,
+      value: priceInput.value,
+    }
+    console.log(productObj)
+
     productInput.value = "";
   }
 }
 
-function deleteProduct(event) {
+//####################################################################################
+
+
+function productInteration(event) {
   const productItem = event.target;
 
   if (productItem.classList[0] === "remove-product") {
     const productParent = productItem.parentElement;
     removeProductLocal(productParent);
     productParent.remove();
-  }
+    const subtractionButton = document.querySelector(".remove-product");
+    subtractionButton.addEventListener("click", subtractTotal);
+    subtractTotal();
 
-  //mostra popup digitar preço
+  }
   if (productItem.classList[0] === "checked-product") {
     document.querySelector(".insert-price-container").classList.add("show-price-area");
     const productParent = productItem.parentElement;
@@ -74,9 +89,10 @@ function deleteProduct(event) {
   }
 }
 
+//####################################################################################
+
 function sendPrice() {
   const productPrice = priceInput.value.trim();
-  console.log(productPrice)
 
   if (!productPrice) {
     var emptyMessage = "O campo preço não pode estar vazio";
@@ -85,20 +101,38 @@ function sendPrice() {
     element.classList.add("show-modal-empty-text");
     message.innerText = emptyMessage;
   } else {
-    //lógica para atribuir preço ao produto e somar ao total...
+
+    
+
     document.querySelector(".insert-price-container").classList.remove("show-price-area");
-
-
     var subResult = parseFloat(totalValueList) + parseFloat(productPrice);
-
     totalFinalValueList += subResult;
     document.querySelector("p").innerText = "R$ " + totalFinalValueList.toFixed(2);
-    //addLocalProductsPrice(totalFinalValueList);
-    //priceInput = 0;
-    console.log("Teste: " + testotalFinalValueListte);
 
+    //jogar dentro de um for? Usar Storeage?
+    //testar dentro do remove, pensar em como deletar o item(index) certo na lista
+    //verificar a soma...
+    productObj.value = productPrice;
+
+    productArray.push(productObj);
+
+    console.log(productArray);
   }
 }
+
+//####################################################################################
+
+//Criar array de objetos de produtos
+function subtractTotal(){
+  const productPrice = priceInput.value.trim();
+
+  var subResult = parseFloat(totalFinalValueList) - parseFloat(productPrice);
+  
+  document.querySelector("p").innerText = "R$ " + subResult.toFixed(2);
+  totalFinalValueList = 0;
+}
+
+//####################################################################################
 
 // fazer um storeage para precos, verificar se é pra fazer em um método separado
 //verificar, está dando null
@@ -115,6 +149,8 @@ function addLocalProductsPrice(addProductPriceLocal) {
   localStorage.setItem("addProductsPrice", JSON.stringify(addProductsPrice));
 }
 
+//####################################################################################
+
 function addLocalProducts(addProductLocal) {
   let addProducts;
 
@@ -127,6 +163,8 @@ function addLocalProducts(addProductLocal) {
   addProducts.push(addProductLocal);
   localStorage.setItem("addProducts", JSON.stringify(addProducts));
 }
+
+//####################################################################################
 
 function getProducts() {
   let addProducts;
@@ -161,6 +199,8 @@ function getProducts() {
   });
 }
 
+//####################################################################################
+
 function removeProductLocal(product) {
   let addProducts;
 
@@ -177,20 +217,9 @@ function removeProductLocal(product) {
   localStorage.setItem("addProducts", JSON.stringify(addProducts));
 }
 
+//####################################################################################
+
 function hidenModalAdvice() {
   var element = document.querySelector(".modal-empty-text");
   element.classList.remove("show-modal-empty-text");
 }
-
-// function showPriceArea(){
-
-//     var element = document.querySelector('.insert-price-container');
-//     element.classList.add('.title-price');
-//     element.classList.add('show-price-area');
-//     // element.innerText =
-// }
-
-// function writeItAdviceEmptyText(text){
-//     var element = "O nome do produto não pode estar vazio."
-//     document.write(element);
-// }
