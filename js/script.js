36a341300f4ad8303dd9cf5bd8413fd4ecd58231
Eduardo <eduardoamorim927@gmail.com>
2022-05-04 11:51:52 -0300

    Foram feitos testes com inserção de preços, foi inserido a lista suspensa para filtrar status dos produtos

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ class Product {
     this.id = 1;
     this.price = 0;
     this.arrayProducts = [];
+    this.editProductPrice = null;
   }
 
   save() {
@@ -58,6 +59,7 @@ class Product {
     let productItem = {};
 
     productItem.id = this.id;
+    //    productItem.price =  document.getElementById("price-input").value;
     productItem.price = this.price;
     productItem.name = document.getElementById("product-name").value;
     return productItem;
@@ -80,7 +82,14 @@ class Product {
     }
   }
 
-  validationPriceArea(product) {
+  validationPriceArea(event) {
+
+    //event.preventDefault();
+    const priceButton = document.querySelector(".price-button");
+    priceButton.addEventListener("click", this.checkedProductLine);
+
+    //const productPrice = priceInput.value.trim();
+
     const productInputPrice = document.querySelector("#price-input");
     const productPrice = productInputPrice.value.trim();
 
@@ -119,28 +128,50 @@ class Product {
   }
 
   checkedProductLine(index){
-    var element = document.querySelector(".insert-price-container");
-    const productDiv = document.createElement("div");
-    element.classList.add("show-price-area");
+    
+    //document.querySelector(".product-price").value = index.price;
+    // var element = document.querySelector(".insert-price-container");
+    document.querySelector(".insert-price-container").classList.add("show-price-area");
+    //const productDiv = document.createElement("div");
+    // element.classList.add("show-price-area");
     let product = this.getItens();
+
+    document.querySelector(".product").classList.add("completed");
+
+    const priceButton = document.querySelector(".price-button");
+    priceButton.addEventListener("click", this.hidePriceArea);
     
     for (let i = 0; i < this.arrayProducts.length; i++) {
 
-      if(!this.validationPriceArea(product.price)){
-        console.log("entrou");
-        var emptyMessage = "O campo preço não pode estar vazio";
-        var element = document.querySelector(".modal-empty-text");
-        var message = document.querySelector(".modal-empty-text-message");
-        element.classList.add("show-modal-empty-text");
-        message.innerText = emptyMessage;
+      // if(this.validationPriceArea(product.price)){
         
-      } else {
-        if(this.arrayProducts[i].id === index){
+      //   console.log("entrou");
+      //   var emptyMessage = "O campo preço não pode estar vazio";
+      //   var element = document.querySelector(".modal-empty-text");
+      //   var message = document.querySelector(".modal-empty-text-message");
+      //   element.classList.add("show-modal-empty-text");
+      //   message.innerText = emptyMessage;
         
-          console.log(this.arrayProducts[i].name);
-        }
-      }
+      // } else {
+
+      //   console.log("No else, depois de entrar: " + product.price);
+      //   if(this.arrayProducts[i].id === index){
+        
+      //     console.log(this.arrayProducts[i].name);
+      //   }
+      // }
     }
+  }
+
+  hidePriceArea(event){
+    event.preventDefault();
+
+    var productPrice = document.getElementById("price-input").value;
+    console.log(productPrice)
+    document.querySelector(".product-price").innerText = productPrice;
+    
+    document.querySelector(".insert-price-container").classList.remove("show-price-area");
+    
   }
 }
 
